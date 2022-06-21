4de0ff6274435b3363f3cb6770debde7091be337
taiprogrammer <taiza.marques@sptech.school>
2022-06-21 11:20:25 -0300

    fetching products json to (ListProducts) component

---
+++ src/components/ListProducts.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="produtos-container">
+    <div v-for="(product, index) in products" :key="index">
+      <img
+        v-if="product.fotos"
+        :src="product.fotos[0].src"
+        :alt="product.nome[0]"
+      />
+      <p class="preco">{{ product.preco }}</p>
+      <h2 class="titulo">{{ product.nome }}</h2>
+      <p class="descricao">{{ product.descricao }}</p>
+      <!-- {{ product }} -->
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      products: new Array(),
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3000/produtos")
+        .then((response) => response.json())
+        .then((response) => {
+          this.products = response;
+        });
+    },
+  },
+  mounted() {
+    this.getProdutos();
+  },
+};
+</script>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
     <header-component />
+    <section>
+      <h1>Compre ou Venda</h1>
+      <search-component />
+      <list-products />
+    </section>
     <footer-component />
   </div>
 </template>
 
 <script>
+import ListProducts from "@/components/ListProducts.vue";
+import SearchComponent from "@/components/SearchComponent.vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
 export default {
   name: "HomeView",
   components: {
+    ListProducts,
     HeaderComponent,
     FooterComponent,
+    SearchComponent,
   },
 };
 </script>
+<style scoped>
+h1 {
+  color: #87f;
+  font-size: 2rem;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
