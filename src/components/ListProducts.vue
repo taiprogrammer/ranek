c4d1f544308ba0e63e562955570ba8acaadf53c6
taiprogrammer <taiza.marques@sptech.school>
2022-06-22 10:41:06 -0300

    styling product component

--- src/components/ListProducts.vue
+++ src/components/ListProducts.vue
@@ -1,14 +1,21 @@
 <template>
   <section class="produtos-container">
-    <div v-for="(product, index) in products" :key="index">
-      <img
-        v-if="product.fotos"
-        :src="product.fotos[0].src"
-        :alt="product.nome[0]"
-      />
-      <h2 class="titulo">{{ product.nome }}</h2>
-      <p class="preco">{{ product.preco }}</p>
-      <p class="descricao">{{ product.descricao }}</p>
+    <div v-if="products && products.length > 0" class="produtos">
+      <div v-for="(product, index) in products" :key="index" class="produto">
+        <router-link to="/">
+          <img
+            v-if="product.fotos"
+            :src="product.fotos[0].src"
+            :alt="product.nome[0]"
+          />
+          <h2 class="titulo">{{ product.nome }}</h2>
+          <p class="preco">{{ product.preco }}</p>
+          <p class="descricao">{{ product.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="product && products.length === 0">
+      <p>Nenhum produto encontrado</p>
     </div>
   </section>
 </template>
@@ -51,4 +58,48 @@ export default {
   },
 };
 </script>
-<style></style>
+<style scoped>
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.01);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+}
+</style>
