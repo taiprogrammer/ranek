a5ad14540d2040b88fa735b9d929ffaa310910bd
taiprogrammer <taiza.marques@sptech.school>
2022-06-28 11:56:31 -0300

    feature (UserForm) - new component fo register of user; chore (CreateLoginComponent) - calling the new component of form to the component of registering user

--- src/components/CreateLoginComponent.vue
+++ src/components/CreateLoginComponent.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>Login</div>
+  <section>
+    <h2>Crie a sua conta</h2>
+    <transition mode="out-in">
+      <button v-if="!create" class="btn" @click="create = true">
+        Criar conta
+      </button>
+      <user-form v-else>
+        <template>
+          <button class="btn">Criar conta</button>
+        </template>
+      </user-form>
+    </transition>
+  </section>
 </template>
 <script>
+import UserForm from "@/components/UserForm.vue";
 export default {
   name: "CreateLoginComponent",
+  data() {
+    return {
+      create: false,
+    };
+  },
+  components: {
+    UserForm,
+  },
 };
 </script>
-<style scoped></style>
+<style scoped>
+h2 {
+  margin-top: 40px;
+  margin-bottom: 10px;
+}
+
+.btn {
+  width: 100%;
+  max-width: 300px;
+}
+</style>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,55 @@
+<template>
+  <form>
+    <label for="name">Nome</label>
+    <input type="text" id="name" name="name" v-model="name" />
+    <label for="email">Email</label>
+    <input type="text" id="email" name="email" v-model="email" />
+    <label for="password">Senha</label>
+    <input type="password" id="password" name="password" v-model="password" />
+    <label for="cep">Cep</label>
+    <input type="text" id="cep" name="cep" v-model="cep" />
+    <label for="street">Rua</label>
+    <input type="text" id="street" name="street" v-model="street" />
+    <label for="number">Número</label>
+    <input type="text" id="number" name="number" v-model="number" />
+    <label for="neighbourhood">Bairro</label>
+    <input
+      type="text"
+      id="neighbourhood"
+      name="neighbourhood"
+      v-model="neighbourhood"
+    />
+    <label for="city">Cidade</label>
+    <input type="text" id="city" name="city" v-model="city" />
+    <label for="state">Estado</label>
+    <input type="text" id="state" name="state" v-model="state" />
+    <div class="button">
+      <slot></slot>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "UserForm",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      cep: "",
+      street: "",
+      number: "",
+      neighbourhood: "",
+      city: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: grid;
+  margin-bottom: 20px;
+}
+</style>
